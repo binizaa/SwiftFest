04d5ff01c965892e5c037485032ea3d031195abb
Kevin <kevings2004@hotmail.com>
2025-06-07 10:21:47 -0600

    Initial Commit

---
+++ swiftchallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftchallenge
+//
+//  Created by Kevin Garcia on 07/06/25.
+// d
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftchallenge/swiftchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftchallengeApp.swift
+//  swiftchallenge
+//
+//  Created by Kevin Garcia on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
