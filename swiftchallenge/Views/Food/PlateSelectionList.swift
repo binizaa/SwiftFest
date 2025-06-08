39d9b40d3485f6aedf2013deb0c347bb276d390f
David-Brnb <321171197@ciencias.unam.mx>
2025-06-08 05:05:53 -0600

    add: detected food view

---
+++ swiftchallenge/Views/Food/PlateSelectionList.swift
@@ -0,0 +1,18 @@
+//
+//  PlateSelectionList.swift
+//  swiftchallenge
+//
+//  Created by Leoni Bernabe on 08/06/25.
+//
+
+import SwiftUI
+
+struct PlateSelectionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlateSelectionList()
+}
